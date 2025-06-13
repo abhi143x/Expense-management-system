aa767789babca96f1f6b0eb5ae9f04122ca0b7d3
abhi143x <iabhirajverma35@gmail.com>
2025-06-13 14:27:35 +0530

    created Authentication APIs(login, signup, getuser, photo upload)

---
+++ Backend/server.cjs
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+const connectDB = require("./config/db.cjs");
+const authRoutes = require("./routes/authRoutes.cjs")
+
+const app = express()
+
+//Midddleware to handle CORS
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes)
+
+//Server uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
