1c2eb97830d4780d4214f797f0cf7ee8edd45cf8
abhi143x <iabhirajverma35@gmail.com>
2025-07-16 15:26:23 +0530

    created Expense API

--- Backend/server.cjs
+++ Backend/server.cjs
@@ -5,6 +5,7 @@ const path = require("path")
 const connectDB = require("./config/db.cjs");
 const authRoutes = require("./routes/authRoutes.cjs")
 const incomeRoutes = require("./routes/incomeRoutes.cjs")
+const expenseRoutes = require("./routes/expenseRoutes.cjs")
 
 const app = express()
 
@@ -23,6 +24,7 @@ connectDB();
 
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/income", incomeRoutes)
+app.use("/api/v1/expense", expenseRoutes)
 
 //Server uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
