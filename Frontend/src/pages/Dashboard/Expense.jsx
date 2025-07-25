a9ec1418f87facfdb177f45157120904c4c49bf3
abhi143x <iabhirajverma35@gmail.com>
2025-07-25 22:52:11 +0530

    feature: expense overvie and add expense

--- Frontend/src/pages/Dashboard/Expense.jsx
+++ Frontend/src/pages/Dashboard/Expense.jsx
@@ -1,11 +1,113 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useUserAuth } from "../../hooks/UseUserAuth";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { API_PATHS } from "../../utils/apiPaths.cjs";
+import axiosInstance from "../../utils/axiosInstanse.cjs";
+import toast from "react-hot-toast";
+import ExpenseOverview from "../../components/Expense/ExpenseOverview";
+import Modal from "../../components/Modal";
+import AddExpenseForm from "../../components/Expense/AddExpenseForm";
 
 const Expense = () => {
-  return (
-    <div>
-      Exoemse
-    </div>
-  )
-}
+  useUserAuth();
 
-export default Expense
+  const [expenseData, setExpenseData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
+    show: false,
+    data: null,
+  });
+
+  const [openAddExpenseModal, setOpenAddExpenseModal] = useState(false);
+
+  //Get all expense details
+  const fetchExpenseDetails = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.EXPENSE.GET_ALL_EXPENSE}`
+      );
+
+      if (response.data) {
+        setExpenseData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong, Please Try again. Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  //handle add expense
+  const handleAddExpense = async (expense) => {
+    const { category, amount, date, icon } = expense;
+
+    //validation check
+    if (!category.trim()) {
+      toast.error("Category is required");
+      return;
+    }
+
+    if (!amount || isNaN(amount) || Number(amount) <= 0) {
+      toast.error("Amount should be a valid number greater than 0.");
+      return;
+    }
+
+    if (!date) {
+      toast.error("Date is required");
+    }
+
+    try {
+      await axiosInstance.post(API_PATHS.EXPENSE.ADD_EXPENSE, {
+        category,
+        amount,
+        date,
+        icon,
+      });
+      setOpenAddExpenseModal(false);
+      toast.success("Expense added successfully");
+      fetchExpenseDetails();
+    } catch (error) {
+      console.error(
+        "Error adding expense",
+        error.response?.data?.message || error.message
+      );
+    }
+  };
+
+  useEffect(() => {
+    fetchExpenseDetails();
+
+    return () => {};
+  }, []);
+
+  return (
+    <DashboardLayout activeMenu="Expense">
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 gap-6">
+          <div className="">
+            <ExpenseOverview
+              transactions={expenseData}
+              onExpenseIncome={() => setOpenAddExpenseModal(true)}
+            />
+          </div>
+        </div>
+
+        <Modal
+          isOpen={openAddExpenseModal}
+          onClose={() => setOpenAddExpenseModal(false)}
+          title="Add Expense"
+        >
+          <AddExpenseForm onAddExpense={handleAddExpense} />
+        </Modal>
+
+
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Expense;
--- Frontend/src/utils/helper.cjs
+++ Frontend/src/utils/helper.cjs
@@ -1,44 +1,58 @@
 import moment from "moment";
 
-export const validateEmail = (email) =>{
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-}
-
-export const getInitials = (name) => {
-    if (!name) return "";
-
-    const words = name.split(" ");
-    let initials  = "";
-
-    for (let i=0; i<Math.min(words.length, 2); i++) {
-        initials += words[i][0];
-    }
-    return initials.toUpperCase();
+export const validateEmail = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
 };
 
+export const getInitials = (name) => {
+  if (!name) return "";
+
+  const words = name.split(" ");
+  let initials = "";
+
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
+    initials += words[i][0];
+  }
+  return initials.toUpperCase();
+};
 
 export const addThousandsSeparator = (num) => {
-    if (num == null || isNaN(num)) return "";
+  if (num == null || isNaN(num)) return "";
 
-    return Number(num).toLocaleString("en-IN");
+  return Number(num).toLocaleString("en-IN");
 };
 
 export const prepareExpenseBarCharData = (data) => {
-    const charData = data.map((item) => ({
-      category: item?.category,
-      amount: Number(item?.amount),
-    }));
-    return charData;
+  const charData = data.map((item) => ({
+    category: item?.category,
+    amount: Number(item?.amount),
+  }));
+  return charData;
 };
 
 export const prepareIncomeBarChartData = (data = []) => {
-    const sortedData = [...data].sort((a,b) => new Date(a.date) - new Date(b.date));
+  const sortedData = [...data].sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
 
-    const charData = sortedData.map((item) => ({
-        month: moment(item?.date).format("Do MMM"),
-        amount: item?.amount,
-        source: item?.source,
-    }));
-    return charData;
-}
+  const charData = sortedData.map((item) => ({
+    month: moment(item?.date).format("Do MMM"),
+    amount: item?.amount,
+    source: item?.source,
+  }));
+  return charData;
+};
+
+export const prepareExpenseLineChartData = (data = []) => {
+  const sortedData = [...data].sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
+  const charData = sortedData.map((item) => ({
+    month: moment(item?.date).format("Do MMM"),
+    amount: item?.amount,
+    category: item?.category,
+  }));
+  return charData;
+};
