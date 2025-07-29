f37c07338dc2b9b2ffd7b80bea26c7d6347a1b81
abhi143x <iabhirajverma35@gmail.com>
2025-07-29 16:09:44 +0530

    readme

--- Frontend/src/components/Income/IncomeList.jsx
+++ Frontend/src/components/Income/IncomeList.jsx
@@ -14,7 +14,7 @@ const IncomeList = ({ transactions, onDelete, onDownload }) => {
         </button>
       </div>
 
-      <div className="grid grid-cols-1 md:gird-cols-2">
+      <div className="grid grid-cols-1 md:grid-cols-2">
         {transactions?.map((income) => (
           <TransactionInfoCard
             key={income._id}
