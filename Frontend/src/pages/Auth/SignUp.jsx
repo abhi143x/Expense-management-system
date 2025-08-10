7401a57fe83f980a5e7ca834e82af1688e2258e6
abhi143x <iabhirajverma35@gmail.com>
2025-08-10 17:34:34 +0530

    some changes for deployment!!

--- Frontend/src/pages/Auth/SignUp.jsx
+++ Frontend/src/pages/Auth/SignUp.jsx
@@ -15,112 +15,122 @@ const SignUp = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const [error, setError] = useState(null);
 
-  const { updateUser } = useContext(UserContext)
+  const { updateUser } = useContext(UserContext);
   const navigate = useNavigate();
 
   const handleSignUp = async (e) => {
     e.preventDefault();
-
     let profileImageUrl = "";
 
     if (!fullName) {
-      setError("Please enter your name");
+      setError("Please enter your name.");
       return;
     }
-
     if (!validateEmail(email)) {
       setError("Please enter a valid email address.");
       return;
     }
-
     if (!password) {
-      setError("Please enter the password");
+      setError("Please enter the password.");
       return;
     }
 
     setError("");
 
-    // SignUp API call 
     try {
-
-      //Upload image if present 
       if (profilePic) {
         const imgUploadRes = await uploadImage(profilePic);
         profileImageUrl = imgUploadRes.imageUrl || "";
-      } 
-
+      }
 
       const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
         fullName,
         email,
         password,
-        profileImageUrl
+        profileImageUrl,
       });
+
       const { token, user } = response.data;
 
       if (token) {
         localStorage.setItem("token", token);
         updateUser(user);
-        navigate("/dashboard");
+
+        //Show success alert
+        alert("Account Created Successfully!");
+
+        //Redirect to login page
+        navigate("/login");
       }
-    } catch ( error ) {
-      if (error.response && error.response.data.message ) {
+    } catch (error) {
+      if (error.response && error.response.data.message) {
         setError(error.response.data.message);
       } else {
-        setError("Something went wrong. Please try again")
+        setError("Something went wrong, Please try again.");
       }
     }
   };
 
   return (
     <AuthLayout>
-      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
-        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
-        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+      <div className="lg:w-[100%] h-auto md:h-full mt-10  md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl xs:text-2xl font-semibold text-black">
+          Create an Account
+        </h3>
+        <p className="text-xs xs:text-base text-slate-700 mt-[5px] mb-6">
           Join us today by entering your details below.
         </p>
 
-        <form onSubmit={handleSignUp}>
-          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+        <form onSubmit={handleSignUp} >
+          
+            <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+          
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <Input
               value={fullName}
               onChange={({ target }) => setFullName(target.value)}
-              placeholder="John"
               label="Full Name"
+              placeholder="John"
               type="text"
             />
 
             <Input
+              type="text"
               value={email}
               onChange={({ target }) => setEmail(target.value)}
               label="Email Address"
               placeholder="john@example.com"
-              type="text"
             />
-            <div className="col-span-2">
+
+            <div className="md:col-span-2">
               <Input
+                type="password"
                 value={password}
                 onChange={({ target }) => setPassword(target.value)}
                 label="Password"
-                placeholder="Min 8 Characters"
-                type="password"
+                placeholder="Minimum 8 Characters"
               />
             </div>
           </div>
+
           {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
 
-          <button type="submit" className="btn-primary">
-            SIGN UP
+          <button
+            type="submit"
+            className="btn-primary cursor-pointer w-full md:w-auto px-6 py-2"
+          >
+            Sign UP
           </button>
 
-          <p className="text-[13px] text-slate-800 mt-3">
+          <p className="text-[13px] text-slate-800 mt-3 text-center md:text-left">
             Already have an account?{" "}
-            <Link className="font-medium text-primary underline" to="/login">
+            <Link
+              className="font-medium text-primary underline"
+              to="/login"
+            >
               Login
             </Link>
           </p>
